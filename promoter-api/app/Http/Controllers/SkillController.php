9a33214e4107d2b6a4eb56d22592d24d982bbfcd
Denis Kolokolov <dennis.kolokolov@gmail.com>
2024-11-23 19:09:29 +0100

    feat(skill): add CRUD functionality for skills with validation
    - Implemented index, show and store methods in SkillController to handle listing, creation, and retrieval of skills.
    - Added StoreOrUpdateSkillRequest for validation, including name, description, and promoter_group_ids.
    - Included relationship handling for promoterGroups in skill creation.
    - Ensured validation errors return detailed JSON responses.

--- promoter-api/app/Http/Controllers/SkillController.php
+++ promoter-api/app/Http/Controllers/SkillController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreOrUpdateSkillRequest;
 use App\Models\Skill;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class SkillController extends Controller
@@ -10,17 +12,31 @@ class SkillController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): JsonResponse
     {
-        //
+        $skills = Skill::with(['promoters', 'promoterGroups'])->get();
+
+        return response()->json([
+            "ok" => true,
+            "skills" => $skills
+        ], 200);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreOrUpdateSkillRequest $request): JsonResponse
     {
-        //
+        $skill = Skill::create($request->only(['name', 'description']));
+
+        if ($request->has('promoter_group_ids')) {
+            $skill->promoterGroups()->sync($request->input('promoter_group_ids'));
+        }
+
+        return response()->json([
+            'message' => 'Skill created successfully',
+            'skill' => $skill->load('promoterGroups'),
+        ], 201);
     }
 
     /**
@@ -28,7 +44,12 @@ class SkillController extends Controller
      */
     public function show(Skill $skill)
     {
-        //
+        $skill->load(['promoters', 'promoterGroups']);
+
+        return response()->json([
+            "ok" => true,
+            "skill" => $skill
+        ], 200);
     }
 
     /**
